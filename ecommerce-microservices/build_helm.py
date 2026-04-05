3e8673551d60a0b2d1c1e39bef737bebe4176c88
Vignesh <91901@example.com>
2026-04-05 16:27:15 +0530

    update

--- ecommerce-microservices/build_helm.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-if __name__ == '__main__':
-    script_path = os.path.join('scripts', 'generate_helm.py')
-    print(f"Redirecting to the new Umbrella Chart generator at {script_path}...")
-    # Execute the actual script
-    with open(script_path, 'r') as f:
-        exec(f.read())
